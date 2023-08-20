3b6170aeaa9453bc86db4e4d4791a43eaf688c3d
KaniyanAhmad <kaniyanahmad1@gmail.com>
2023-08-20 09:12:32 -0700

    Basic Skeleton

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+// This code will remove all of the divs inside the gridContainer
+// const gridContainer = document.getElementById('gridContainer');
+
+// button.onclick = function () {
+//     while (gridContainer.hasChildNodes()) {
+//         gridContainer.removeChild(gridContainer.firstChild);
+//       }
+// }
+
+
+    
