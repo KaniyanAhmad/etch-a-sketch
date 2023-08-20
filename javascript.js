d97d77e55d238aef1d81980a8e0a357546b33f35
KaniyanAhmad <kaniyanahmad1@gmail.com>
2023-08-20 10:45:50 -0700

    Grid generator in js

--- javascript.js
+++ javascript.js
@@ -9,4 +9,24 @@
 // }
 
 
-    
+
+//div generator
+
+const gridContainer = document.getElementById('gridContainer');
+
+let x = 64;
+gridContainer.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+
+for (let row = 0; row < x; row++) {
+    for (let column = 0; column < x; column++) {
+        const box = document.createElement('div');
+        box.classList.add('gridItem');
+        gridContainer.appendChild(box);
+
+        box.addEventListener('mouseover', function () {
+            console.log('in event listener');
+            box.style.backgroundColor = 'black';
+        });
+    }
+}
+
