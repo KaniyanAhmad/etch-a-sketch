0e6ab21a216e3e601bb07f81d130fb83b4f942a8
KaniyanAhmad <kaniyanahmad1@gmail.com>
2023-08-21 11:34:28 -0700

    Grid Item Default Color Btn

--- javascript.js
+++ javascript.js
@@ -1,32 +1,51 @@
 
-// This code will remove all of the divs inside the gridContainer
-// const gridContainer = document.getElementById('gridContainer');
-
-// button.onclick = function () {
-//     while (gridContainer.hasChildNodes()) {
-//         gridContainer.removeChild(gridContainer.firstChild);
-//       }
-// }
-
-
-
-//div generator
 
 const gridContainer = document.getElementById('gridContainer');
+const sliderValue = document.getElementById('slider');
+const gridItem = document.querySelectorAll('#gridContainer > *');
 
-let x = 64;
-gridContainer.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+const clearBtn = document.querySelector('#clear > button');
 
-for (let row = 0; row < x; row++) {
-    for (let column = 0; column < x; column++) {
-        const box = document.createElement('div');
-        box.classList.add('gridItem');
-        gridContainer.appendChild(box);
 
-        box.addEventListener('mouseover', function () {
-            console.log('in event listener');
-            box.style.backgroundColor = 'black';
-        });
+let sliderNewValue = sliderValue.value;
+
+//div generator
+function gridGenerator(x) {
+    gridContainer.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+    
+    for (let row = 0; row < x; row++) {
+        for (let column = 0; column < x; column++) {
+            const box = document.createElement('div');
+            box.classList.add('gridItem');
+            gridContainer.appendChild(box);
+    
+            box.addEventListener('mouseover', function () {
+                box.style.backgroundColor = 'black';
+            });
+
+        }
     }
 }
+
+// Grid Remover
+function gridRemover() {
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+      }
+}
+
+// Clear grid colors
+function clearGridColors() {
+    const gridItems = document.querySelectorAll('.gridItem');
+    gridItems.forEach(item => {
+        item.style.backgroundColor = 'white';
+    });
+}
+
+// Grid color Default
+clearBtn.onclick = function () {
+    clearGridColors();
+};
+
+
 
